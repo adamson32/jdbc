8dea6741c50c3bf0d2d9a26afdd8063438093017
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-05-15 20:01:29 +0200

    add method updateEmployee

--- zadanie1_jdbc/src/zadanie1_jdbc/DaoEmployee.java
+++ zadanie1_jdbc/src/zadanie1_jdbc/DaoEmployee.java
@@ -174,4 +174,30 @@ public class DaoEmployee {
             e.printStackTrace();
         }       
     }
+       
+       public void updateEmployee(int prac_id,String prac_imie,String prac_nazwisko,int prac_wiek,String prac_nr_telefon,String prac_email)
+    {
+        String query = "update pracownicy\n" +
+                        "set prac_imie='"+prac_imie+"',prac_nazwisko='"+prac_nazwisko+"',prac_wiek="+prac_wiek+",prac_nr_telefonu='"+prac_nr_telefon+"',prac_email='"+prac_email+"'\n" +
+                        "where prac_id="+prac_id;
+        
+         try {
+             Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            rs.next();
+             Employee p=new Employee();
+             p.setPracId(rs.getInt("prac_id"));
+             p.setPracImie(rs.getString("prac_imie"));
+             p.setPracNazwisko(rs.getString("prac_nazwisko"));
+             p.setPracWiek(rs.getInt("prac_wiek"));
+             p.setPracNrTelefonu(rs.getString("prac_nr_telefonu"));
+             p.setPracEmail(rs.getString("prac_email"));
+            
+             
+         } catch (Exception ex) {
+             System.out.println(ex);
+         }
+        
+        
+    }
 }
